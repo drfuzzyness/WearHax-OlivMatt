9e036676b5afb1b82cf0ff6904c566bd1be3d588
Matthew Conto <mbsconto@gmail.com>
2015-10-24 11:50:28 -0400

    Fists

---
+++ Assets/Scripts/KinectFistController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinectFistController : MonoBehaviour {
+
+	public Transform leftFist;
+	public Transform rightFist;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
