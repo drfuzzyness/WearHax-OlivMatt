c9531505dcacd124519ee09f1692cc81b4ab8ed2
oligb <oligb7@gmail.com>
2015-10-24 10:55:13 -0400

    new stuff

---
+++ Assets/FractureScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FractureScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ActivatePhysics(){
+
+		GetComponent<Rigidbody> ().useGravity = true;
+		GetComponent<MeshCollider> ().enabled = true;
+	}
+}
---
+++ Assets/FragmentScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FragmentScript : MonoBehaviour {
+	
+	// Use this for initialization
+	public GameObject parent;
+	public bool stop=false;
+	public Rigidbody rbody;
+	public Vector3 colPos;
+	public float forceAmt=100f;
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		parent = transform.parent.gameObject;
+
+	}
+
+	public void ActivateFragments(){
+		rbody.useGravity=true;
+		//Vector3 force = (transform.TransformPoint( colPos)-transform.position).normalized;
+		//rbody.AddForce (force * forceAmt);
+	}
+}
