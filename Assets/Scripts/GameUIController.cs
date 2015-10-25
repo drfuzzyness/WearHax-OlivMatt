637e99336bb6a4a49a78e6438433464abc292b5d
Matthew Conto <mbsconto@gmail.com>
2015-10-25 04:04:39 -0400

    HUD UI baby

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameUIController : MonoBehaviour {
+
+	public Text scoreNumTO;
+	public Text timerNumTO;
+	public Text missileTO;
+	private GameManager gm;
+	//missile manager
+
+	// Use this for initialization
+	void Start () {
+		gm = GameManager.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreNumTO.text = gm.score.ToString();
+		timerNumTO.text = gm.timer.ToString();
+		missileTO.color = Color.clear;
+		// update missile text
+		// update missile overlay
+	}
+}
