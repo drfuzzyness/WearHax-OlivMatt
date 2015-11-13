2071c208217f6bf117b7225ba0f32f3082e6eba5
Matthew Conto <mbsconto@gmail.com>
2015-11-13 16:51:57 -0500

    Last minute matt changes

--- Assets/Scripts/MoveAroundTest.cs
+++ Assets/Scripts/MoveAroundTest.cs
@@ -5,6 +5,7 @@ public class MoveAroundTest : MonoBehaviour {
 
 	// Use this for initialization
 	public float speed=1f;
+	public AudioSource stompSFX;
 	public float walkingDuration;
 	public float pauseDuration;
 	public bool moving;
@@ -12,27 +13,39 @@ public class MoveAroundTest : MonoBehaviour {
 	public float timer;
 	void Start () {
 		timer = 0;
-		moving = true;
+		moving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if( moving ) {
-			if( timer <= 0 ) {
-				if( Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0 ) {
-					timer = walkingDuration + pauseDuration;
-				}
-			} else if ( timer < pauseDuration ) {
-				// noting
-			} else  {
+//		if( moving ) {
+//			if( timer <= 0 ) {
+//				if( Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0 ) {
+//					timer = walkingDuration + pauseDuration;
+//				}
+//			} else if ( timer < pauseDuration ) {
+//				// noting
+//			} else  {
 				transform.Translate(Vector3.forward*speed * Input.GetAxis ("Vertical") * Time.deltaTime
 				                    + Vector3.right*speed * Input.GetAxis ("Horizontal") * Time.deltaTime);
+
+//			}
+//			timer -= Time.deltaTime;
+//		}
+
+		if( Input.GetAxis ("Vertical") != 0 || Input.GetAxis ("Horizontal") != 0 ) {
+			if( !moving ) {
+				stompSFX.Play();
 			}
-			timer -= Time.deltaTime;
+		} else {
+			moving = false;
+			if( moving ) {
+				stompSFX.Stop();
+				stompSFX.time = 0f;
+			}
 		}
-
-
+		
 
 
 	}
